acc5d3f8571ec79ba2de5e3fbde9ac80d8d198d0
lemajr <erickbale360@gmail.com>
2024-11-19 22:30:15 +0300

    custom login button

--- app/(auth)/sign-in/[[...sigin-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sigin-in]]/page.tsx
@@ -72,12 +72,41 @@ const Signin = () => {
           {clerkError && (
             <p className="text-red-500 text-sm">{clerkError}</p>
           )}
-          <button
-            type="submit"
-            className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600"
-          >
-            Sign In
-          </button>
+<button
+  type="submit"
+  className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 flex items-center justify-center"
+  disabled={isLoaded}
+>
+  {isLoaded ? (
+    <>
+      <svg
+        className="animate-spin h-5 w-5 mr-3 text-white"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          className="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          strokeWidth="4"
+        ></circle>
+        <path
+          className="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8v4l3-3-3-3v4a8 8 0 018 8H4z"
+        ></path>
+      </svg>
+      Signing in...
+    </>
+  ) : (
+    "Sign In"
+  )}
+</button>
+
+         
         </form>
         <p className="text-sm text-gray-500 text-center mt-4">
           Don’t have an account?{" "}
