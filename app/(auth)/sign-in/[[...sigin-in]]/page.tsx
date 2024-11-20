b1a9ecb75d9e9a136b80c2730332aff0b949ec6c
lemajr <erickbale360@gmail.com>
2024-11-20 12:53:43 +0300

    Grow effect added to Auth pages

--- app/(auth)/sign-in/[[...sigin-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sigin-in]]/page.tsx
@@ -1,14 +1,34 @@
-'use client'
+'use client';
 
 import { SignIn } from "@clerk/nextjs";
 
 
-const Signin = () => {
- return (
- <div className="flex justify-center items-center h-full">
-   <SignIn />
- </div>
- )
-};
+// const Signin = () => {
+//  return (
+//  <div className="flex justify-center items-center h-full bg-red-500">
+//    <SignIn />
+//  </div>
+//  )
+// };
 
-export default Signin;
+// export default Signin;
+
+
+
+import React from "react";
+// import { IconAppWindow } from "@tabler/icons-react";
+import { BackgroundGradient } from "@/components/ui/background-gradient";
+
+
+const page = () => {
+  return (
+    <div className="flex justify-center items-center h-full">
+    <BackgroundGradient className="rounded-2xl bg-white dark:bg-zinc-900">
+      <SignIn />
+
+    </BackgroundGradient>
+  </div>
+  )
+}
+
+export default page
